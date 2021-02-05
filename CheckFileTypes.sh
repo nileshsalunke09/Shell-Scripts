02c3b35fa965e1ea24b4fa4be02ee1fcdcf522f6
nileshsalunke09 <76640503+nileshsalunke09@users.noreply.github.com>
2021-02-05 17:10:11 +0530

    Update CheckFileTypes.sh

--- CheckFileTypes.sh
+++ CheckFileTypes.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
-echo -n "Input the path to Directory: "
-read DIR
-cd $DIR
-find . -type f -ls | awk '{print $11}' > /root/out.txt
-for i in  $(cat /root/out.txt);
-do
-file $i 
-ls -l $i | awk {'print $5'} 
-done
+
+
+# loop & print a folder recusively,
+print_folder_recurse() {
+    for i in "$1"/*;do
+        if [ -d "$i" ];then
+                local temp=$i
+                print_folder_recurse "$temp"
+        elif [ -f "$i" ]; then
+                filename="${1##*/}"
+                extension="${1##*.}"
+                filesize=$(stat -c%b "$1")
+                if [ "${file[$extension]}" ];
+                then
+                        (( file[$extension]++ ))
+                        (( size[$extension]+=$filesize ))
+                else
+                        file[$extension]=1
+                        size[$extension]=$filesize
+                fi
+        fi
+    done
+}
+
+
+declare -A file
+declare -A size
+
+file[txt]=0
+size[html]=0
+
+
+# try get path from param
+path=""
+if [ -d "$1" ]; then
+    path=$1;
+else
+    path="/tmp"
+fi
+
+echo "base path: $path"
+print_folder_recurse $path
+
+for i in "${!file[@]}";
